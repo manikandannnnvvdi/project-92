19c0327d43613d0af7564946fca58a004a81bfc4
manikandannnnvvdi <85862265+manikandannnnvvdi@users.noreply.github.com>
2022-12-22 12:04:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAO5HhU0O2osoi2ECI0ZJb8pQKbNQoHbdY",
+  authDomain: "kidster-3af39.firebaseapp.com",
+  projectId: "kidster-3af39",
+  storageBucket: "kidster-3af39.appspot.com",
+  messagingSenderId: "812653840073",
+  appId: "1:812653840073:web:4f104ea73ada4883c5af18"
+};
+
+firebaseConfig.database = initializeApp(firebaseConfig);
